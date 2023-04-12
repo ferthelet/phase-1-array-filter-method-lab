3d8faa2728d9e81743704a53d305364a235d05a2
Juan Salazar <ferthelet@gmail.com>
2023-04-12 10:04:54 -0500

    adds functions hearders and initial test result

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+// js
+
+// write functions using the filter() method so that PickMeUp Taxi
+// service employees can easily query the data.
+
+function findMatching() {
+    // takes an array of drivers' names and a string as arguments,
+    // and returns the matching list of drivers. The function should 
+    // be case insensitive.
+}
+
+function fuzzyMatch() {
+    // takes an array of drivers' names and a string as arguments
+    // for querying the array, and returns all drivers whose names
+    // begin with the provided letters.
+}
+
+function matchName() {
+    // takes an array of driver objects and a string as arguments.
+    // Each driver object has two properties: name and hometown. The
+    // function should return each element whose name property 
+    // matches the provided string argument.
+
+}
+
